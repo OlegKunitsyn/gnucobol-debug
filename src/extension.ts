69de62ead96b95db2d587094f1a7ec1f44db8969
Bruno Pacheco <brunopacheco1@yahoo.com>
2020-05-04 23:12:28 +0200

    Getting only the major version.

--- src/extension.ts
+++ src/extension.ts
@@ -10,9 +10,9 @@ export function activate(context: vscode.ExtensionContext) {
 class GdbConfigurationProvider implements vscode.DebugConfigurationProvider {
     resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
         const stdout = ChildProcess.spawnSync(config.cobcpath, ['-v'], { cwd: config.wd, env: config.procEnv }).stdout.toString();
-        const match = /cobc\s\(GnuCOBOL\)\s([0-9]+)\.([0-9]+)\.[0-9]+/ig.exec(stdout);
+        const match = /cobc\s\(GnuCOBOL\)\s([0-9]+)/ig.exec(stdout);
         if (match) {
-            config.cobcver = match[1] + match[2];
+            config.cobcver = match[1];
         }
         return config;
     }
