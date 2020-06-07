21f99bf68360acce1195e8c6217445731560de9e
Bruno Pacheco <brunopacheco1@yahoo.com>
2020-06-08 00:14:40 +0200

    #44 - Fixing parser and adding found variables to an array for further evaluation.

--- src/parser.expression.ts
+++ src/parser.expression.ts
@@ -4,21 +4,40 @@ const number = /[0-9]/;
 const numberOrChar = /[a-z0-9]/i;
 const containsCharacter = /(?=.*[a-zA-Z]).*/;
 
-function checkToken(token: string, tokenStack: string[], sourceMap: SourceMap): string {
+function checkToken(token: string, tokenStack: string[], functionName: string, sourceMap: SourceMap, variablesInC: Set<string>): string {
     if (token.length === 0) {
         return "";
     }
 
     if (containsCharacter.test(token)) {
-        //TODO - Fetch variable
-        console.log(`${token} is an identifier!`);
+        let position = -1;
+        let variableName = token;
+        do {
+            const variable = sourceMap.findVariableByCobol(functionName, variableName);
+            if (variable) {
+                variablesInC.add(variable.cName);
+                tokenStack.push(variable.cName);
+                if (position !== -1) {
+                    tokenStack.push(token.substring(position));
+                }
+                return "";
+            }
+
+            position = variableName.lastIndexOf(".", position);
+
+            if (position !== -1) {
+                variableName = variableName.substring(0, position);
+            }
+        } while (position !== -1);
     }
     tokenStack.push(token);
     return "";
 }
 
-export function parseExpression(expression: string, sourceMap: SourceMap): string {
+export function parseExpression(expression: string, functionName: string, sourceMap: SourceMap): [string, string[]] {
     const tokenStack: string[] = [];
+    const variableInC: Set<string> = new Set();
+
     let token = "";
     let openQuote = false;
     let quoteMarker = null;
@@ -29,25 +48,28 @@ export function parseExpression(expression: string, sourceMap: SourceMap): strin
             if (char === quoteMarker) {
                 tokenStack.push(token);
                 openQuote = false;
+                token = "";
             }
             continue;
         }
         switch (char) {
-            case '\s':
+            case ' ':
             case '\t':
             case '\n':
+                token = checkToken(token, tokenStack, functionName, sourceMap, variableInC);
                 continue;
             case '\'':
             case '"':
                 quoteMarker = char;
                 token += char;
                 openQuote = true;
+                continue;
             case '(':
             case ')':
             case '/':
             case '*':
             case ',':
-                token = checkToken(token, tokenStack, sourceMap);
+                token = checkToken(token, tokenStack, functionName, sourceMap, variableInC);
                 tokenStack.push(char);
                 continue;
             case '+':
@@ -55,7 +77,7 @@ export function parseExpression(expression: string, sourceMap: SourceMap): strin
                     token += char;
                     continue;
                 }
-                token = checkToken(token, tokenStack, sourceMap);
+                token = checkToken(token, tokenStack, functionName, sourceMap, variableInC);
                 tokenStack.push(char);
                 continue;
             case '-':
@@ -63,13 +85,13 @@ export function parseExpression(expression: string, sourceMap: SourceMap): strin
                     token += char;
                     continue;
                 }
-                token = checkToken(token, tokenStack, sourceMap);
+                token = checkToken(token, tokenStack, functionName, sourceMap, variableInC);
                 tokenStack.push(char);
                 continue;
             default:
                 token += char;
         }
     }
-    checkToken(token, tokenStack, sourceMap);
-    return tokenStack.join(" ");
+    checkToken(token, tokenStack, functionName, sourceMap, variableInC);
+    return [tokenStack.join(" "), Array.from(variableInC)];
 }
--- test/parser.expression.test.ts
+++ test/parser.expression.test.ts
@@ -1,26 +1,49 @@
 import * as assert from 'assert';
+import * as nativePath from "path";
 import { SourceMap } from '../src/parser.c';
 import { parseExpression } from '../src/parser.expression';
 
 suite("WATCH expression parse", () => {
+    const cwd = nativePath.resolve(__dirname, '../../test/resources');
+    const c = nativePath.resolve(cwd, 'petstore.c');
+    const parsed = new SourceMap(cwd, [c]);
+    console.log(parsed.toString());
+    const functionName = "petstore_";
+
     test("it can parse expressions", () => {
-        const actual = parseExpression("WXSS-AS*3", null);
-        const expected = "WXSS-AS * 3";
+        const [actual, variables] = parseExpression("TOTAL-QUANTITY*3", functionName, parsed);
+        const expected = "f_15 * 3";
         assert.equal(actual, expected);
+        assert.deepEqual(variables, ["f_15"]);
     });
     test("multiple identifiers", () => {
-        const actual = parseExpression("WXSS-AS*WXSS-AS", null);
-        const expected = "WXSS-AS * WXSS-AS";
+        const [actual, variables] = parseExpression("TOTAL-QUANTITY*TOTAL-QUANTITY", functionName, parsed);
+        const expected = "f_15 * f_15";
         assert.equal(actual, expected);
+        assert.deepEqual(variables, ["f_15"]);
     });
     test("only literals", () => {
-        const actual = parseExpression("444.32 - 2232.43", null);
+        const [actual, variables] = parseExpression("444.32 - 2232.43", functionName, parsed);
         const expected = "444.32 - 2232.43";
         assert.equal(actual, expected);
+        assert.deepEqual(variables, []);
     });
     test("it doesn't parse string values", () => {
-        const actual = parseExpression("\"WXSS-AS*WXSS-AS\".substring(0,1)", null);
-        const expected = "\"WXSS-AS*WXSS-AS\" .substring ( 0 , 1 )";
+        const [actual, variables] = parseExpression("\"TOTAL-QUANTITY*TOTAL-QUANTITY\".substring(0,1)", functionName, parsed);
+        const expected = "\"TOTAL-QUANTITY*TOTAL-QUANTITY\" .substring ( 0 , 1 )";
         assert.equal(actual, expected);
+        assert.deepEqual(variables, []);
+    });
+    test("it doesn't parse not found identifiers", () => {
+        const [actual, variables] = parseExpression("BLA-BLA-BLA.BLABLA * 2", functionName, parsed);
+        const expected = "BLA-BLA-BLA.BLABLA * 2";
+        assert.equal(actual, expected);
+        assert.deepEqual(variables, []);
+    });
+    test("it parses complex expressions", () => {
+        const [actual, variables] = parseExpression("\"TOTAL-QUANTITY*TOTAL-QUANTITY\".substring(0,1) + ((2*TOTAL-QUANTITY) - TOTAL-QUANTITY + WS-BILL.TOTAL-COST.length) + (TOTAL-COST.toLowerCase()) + WS-BILL.TOTAL-QUANTITY", functionName, parsed);
+        const expected = "\"TOTAL-QUANTITY*TOTAL-QUANTITY\" .substring ( 0 , 1 ) + ( ( 2 * f_15 ) - f_15 + f_16.length ) + ( f_16.toLowerCase ( ) ) + f_15";
+        assert.equal(actual, expected);
+        assert.deepEqual(variables, ["f_15", "f_16"]);
     });
 });
